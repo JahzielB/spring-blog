8d5b62b8d115030c50edbd29717a450f2fbc642c
JahzielB <jahzielb@gmail.com>
2020-08-03 09:33:51 -0500

    completed first part of views exercise

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,7 +1,10 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,5 +13,21 @@ public class HomeController {
     @ResponseBody
     public String landing() {
         return "This is the landing page!";
+    }
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 }
---
+++ src/main/java/com/codeup/springblog/RollDiceServlet.java
@@ -0,0 +1,32 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class RollDiceServlet{
+    @GetMapping("/roll-dice")
+    public String showRollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{roll}")
+    @ResponseBody
+    public String sayHello(@PathVariable int roll) {
+        int random = (int) (Math.random() * (6 - 1 + 1) + 1);
+        if (roll == random) {
+            return "You guessed: " + roll + "<br>" + "The dice landed on: " + random + "<br>" + "Congrats!";
+        } else {
+            return "You guessed: " + roll + "<br>" + "The dice landed on: " + random + "<br>" + "Sorry";
+        }
+    }
+}
